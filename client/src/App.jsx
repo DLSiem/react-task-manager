6227cab61839d0f7fe63802ff35ccc438a777572
DLSiem <pdlsiem17@gmail.com>
2024-07-08 02:09:13 +0530

    Starting Project

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <p className="text-2xl font-bold text-center">Hello </p>
+      {/* style following paragraph */}
+      <p className="text-lg text-gray-700 leading-relaxed mt-4 text-center items-center px-6">
+        This was also part of the routine. A few seconds of hesitation to see if
+        something else would be added to the order before demuring and saying
+        that would be all.
+      </p>
+      <p className="text-lg font-bold text-gray-700 leading-relaxed mt-4 text-center items-center px-6">
+        He ordered his regular breakfast. Two eggs sunnyside up, hash browns,
+        and two strips of bacon.
+      </p>
+    </>
+  );
+}
+
+export default App;
